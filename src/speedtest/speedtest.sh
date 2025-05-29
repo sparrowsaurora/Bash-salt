488048272bb1e6c5cd37e65937d34a3c648b3b3f
Sparrows <35826584+sparrowsaurora@users.noreply.github.com>
2025-05-29 13:23:24 +0800

    added unix system suport for speedtest package

--- src/speedtest/speedtest.sh
+++ src/speedtest/speedtest.sh
@@ -1,13 +1,11 @@
 function speedtest() {
     # Run executable
-    ~/source/repos/Bash-salt/src/speedtest/speedtest.exe
-
-    # if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    #     echo "wine is needed to run a .exe file"
-    #     wine ~/source/repos/Bash-salt/speedtest/speedtest.exe
-    # elif [[ "$OSTYPE" == "msys" || "$OSTYPE" == "cygwin" || "$OSTYPE" == "win32" ]]; then
-    #     ~/source/repos/Bash-salt/src/speedtest/speedtest.exe
-    # else
-    #     echo "Unsupported OS"
-    # fi
+    if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+        echo "wine is needed to run a .exe file"
+        ~/source/repos/Bash-salt/speedtest/speedtest
+    elif [[ "$OSTYPE" == "msys" || "$OSTYPE" == "cygwin" || "$OSTYPE" == "win32" ]]; then
+        ~/source/repos/Bash-salt/src/speedtest/speedtest.exe
+    else
+        echo "Unsupported OS"
+    fi
 }
