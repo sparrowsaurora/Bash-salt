2327ccde4bbcb7a029de1ec68d375432a57e99d1
Sparrows <35826584+sparrowsaurora@users.noreply.github.com>
2026-01-21 10:59:01 +0800

    finalising installation script

--- src/install.sh
+++ src/install.sh
@@ -2,15 +2,81 @@
 
 set -e # exit on error
 
+# -----------------------
+#  Color Definitions
+# -----------------------
+RED="\e[31m"
+BLUE="\e[36m"
+GREEN="\e[32m" # Success messages
+BOLDBLUE="\e[1;36m" # File names
+ITALICRED="\e[3;31m" # Errors
+UNDERLINE="\033[4m" # Vibes
+ENDCOLOR="\e[0m" # Return
+# Example: echo -e "${ITALICRED}Error: $error_msg${ENDCOLOR}"
+
+
+# -----------------------
+#  Configuration
+# -----------------------
+
+# ALL_PACKAGES=("wordle" "speedtest" "uptime_monitor" "on_startup") # list all packages here
+# INSTALLED_PACKAGES=()
+
 STARTING_DIR=$PWD #save current dir for end process
-LATEST_VERSION=bash-salt-0.6.0.tar.gz
 # echo $STARTING_DIR
+
+LATEST_VERSION=bash-salt-0.6.0.tar.gz
+DOT_DIR=.bash-salt
+
+# Version
+# ?OPTION: dynamically get version from Git, etc.)
+VERSION="0.6.0"
+
+
+# -----------------------
+#  Function Definitions
+# -----------------------
+
+# Make the config.json file based on user input
+# Installs to config_file variable location
+function make_config() {
+    CONFIG_FILE="config.json" # path to config file
+    echo -e "${BLUE}Generating configuration...${ENDCOLOR}"
+    
+    # Username and email
+    read -p "Enter your username: " USERNAME
+    read -p "Enter your email: " EMAIL
+    
+    # Write config to JSON
+    cat > "$CONFIG_FILE" <<EOF
+{
+    "profile": {
+        "username": "$USERNAME",
+        "email": "$EMAIL"
+    },
+    "version": "$VERSION"
+}
+EOF
+    
+    # Success message
+    if [[ -f "$CONFIG_FILE" ]]; then
+        echo -e "${GREEN}Configuration successfully written to${ENDCOLOR} ${BOLDBLUE}${CONFIG_FILE}${ENDCOLOR}"
+    else
+        echo -e "${ITALICRED}Error: Failed to write configuration.${ENDCOLOR}"
+    fi
+}
+
+# -----------------------
+#  Main
+# -----------------------
+
+echo -e "${GREEN}Beginning setup ...${ENDCOLOR}"
 
 # switch directory to install
 # cd $HOME
-cd ./test # !IMPORTANT: remove before prod
-mkdir .bash-salt
-cd .bash-salt
+cd test # !IMPORTANT: remove before prod
+mkdir $DOT_DIR
+cd $DOT_DIR
 
 # Download files + unzip
 echo "Downloading ..."
@@ -27,19 +93,51 @@ rm -rf $LATEST_VERSION
 echo "Downloaded files"
 
 # Link commands to .bashrc
-# TODO: add .aliases & .bashrc to zip
 echo "linking to .bashrc" # ?OPTION: option to rename .bashrc file (-b?)
 # cat <<EOF >> "$HOME/.bashrc"
-cat <<EOF >> "test/.echo_test" # !IMPORTANT: remove before prod
-[ -f "$HOME/.bash-salt/.aliases" ] && source "$HOME/.bash-salt/.aliases"
-[ -f "$HOME/.bash-salt/.bashrc" ] && source "$HOME/.bash-salt/.bashrc"
+cat <<EOF >> ".echo_test" # !IMPORTANT: remove before prod
+[ -f "$HOME/$DOT_DIR/.aliases" ] && source "$HOME/$DOT_DIR/.aliases"
+[ -f "$HOME/$DOT_DIR/.bashrc" ] && source "$HOME/$DOT_DIR/.bashrc"
 EOF
 
+# config file setup
+read -p  "Would you like to create your config file now? (Y/n): " response
+# Default Yes - check for response
+if [[ "$response" =~ ^[Yy]$ || -z "$response" ]]; then
+    make_config
+else
+    echo -e "${UNDERLINE}Some packages Require config.json${ENDCOLOR}"
+    echo -e "${UNDERLINE}Please complete it at your earliest convenience${ENDCOLOR}"
+fi
 
+# add bash commands for packages
+cd $VERSION
+
+dot_sh=(*.sh)
+
+# If the glob didn't match anything
+if [[ ${dot_sh[0]} == "*.sh" ]]; then
+    dot_sh=()
+fi
+
+for file_path in "${dot_sh[@]}"; do
+    echo "Adding $file_path shell config command"
+    # source files on terminal session
+    # 1. copy contents to .bash-salt/$version/.bashrc
+
+    echo >> .bashrc
+    echo >> .bashrc
+    cat "$file_path" >> .bashrc
+
+    # 2. delete?
+    # rm -rf $filepath #????
+done
+
+cd ..
 
 #exit
 cd $STARTING_DIR
-echo "Successfully installed"
+echo -e "${GREEN}Setup Successful${ENDCOLOR}"
 
  exit 1
 fi
--- src/reset.sh
+++ src/reset.sh
@@ -1,8 +1,1 @@
-bashrc_file="./test/bashrc.txt"
-echo "" > $bashrc_file
-
-aliases_file="./test/aliases.txt"
-echo "" > $aliases_file
-
-config_file="./test/config.json"
-echo "" > $config_file
+rm -rf test/.bash-salt
