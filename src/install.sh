9d17b703d6250b9a015c84996c5f49fef876e7f8
Sparrows <35826584+sparrowsaurora@users.noreply.github.com>
2026-01-21 08:49:45 +0800

    updating .tar to 0.6.0 (added .aliases & psuedo .bashrc)

--- src/install.sh
+++ src/install.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
-CURRENT_DIR=$PWD #save currentdir for end process
-# echo $CURRENT_DIR
+set -e # exit on error
+
+STARTING_DIR=$PWD #save current dir for end process
+LATEST_VERSION=bash-salt-0.6.0.tar.gz
+# echo $STARTING_DIR
 
 # switch directory to install
-# cd ~
+# cd $HOME
 cd ./test # !IMPORTANT: remove before prod
 mkdir .bash-salt
 cd .bash-salt
 
 # Download files + unzip
 echo "Downloading ..."
-curl -O "https://bash-salt.vercel.app/assets/versions/bash-salt-0.5.0.tar.gz"
-tar -xzf bash-salt-0.5.0.tar.gz
+curl -O "https://bash-salt.vercel.app/assets/versions/$LATEST_VERSION"
+tar -xzf $LATEST_VERSION
 # NOTE: tar and curl are default installs for gitbash
 # NOTE: this is why we are avoiding using wget and unzip 
 # NOTE: this is also for future access to unix type systems
@@ -20,12 +23,23 @@ tar -xzf bash-salt-0.5.0.tar.gz
 # -v: Verbose output (optional).
 # -z: Decompress the archive using gzip.
 # -f: Specifies the filename of the archive 
-rm -rf bash-salt-0.5.0.tar.gz
+rm -rf $LATEST_VERSION
 echo "Downloaded files"
+
+# Link commands to .bashrc
+# TODO: add .aliases & .bashrc to zip
+echo "linking to .bashrc" # ?OPTION: option to rename .bashrc file (-b?)
+# cat <<EOF >> "$HOME/.bashrc"
+cat <<EOF >> "test/.echo_test" # !IMPORTANT: remove before prod
+[ -f "$HOME/.bash-salt/.aliases" ] && source "$HOME/.bash-salt/.aliases"
+[ -f "$HOME/.bash-salt/.bashrc" ] && source "$HOME/.bash-salt/.bashrc"
+EOF
+
 
 
 #exit
-cd $CURRENT_DIR
+cd $STARTING_DIR
+echo "Successfully installed"
 
  exit 1
 fi
